c27ff3c0777811fb12e8068ed850e749f9ee8f18
IRLFemboy <justsomenerd2@outlook.com>
2023-12-13 09:42:12 -0800

    rgaegearg
    eagaaeg

--- Assets/Scribtws/TitleButtons.cs
+++ Assets/Scribtws/TitleButtons.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 public class TitleButtons : MonoBehaviour
 {
@@ -13,13 +15,28 @@ public class TitleButtons : MonoBehaviour
     int buttonCount = 2;
     bool canMove = true;
 
+    public GameObject beanut;
+
+    private void Start()
+    {
+        beanut.transform.localPosition = buttons[currentButton].gameObject.transform.localPosition + new Vector3(450, 0, 0);
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].color = Color.gray;
+        }
+        buttons[currentButton].color = Color.white;
+    }
+
     private void Update()
     {
         float vertical = Input.GetAxis("Vertical");
         if (Mathf.Abs(vertical) > .1f && canMove)
         {
             canMove = false;
+            buttons[currentButton].color = Color.grey;
             int button = vertical > 0 ? currentButton-- : currentButton++;
+            beanut.transform.localPosition = buttons[currentButton].gameObject.transform.localPosition + new Vector3(450, 0, 0);
 
             if (currentButton > buttonCount)
             {
@@ -29,10 +46,24 @@ public class TitleButtons : MonoBehaviour
             {
                 currentButton = 0;
             }
+            buttons[currentButton].color = Color.white;
         }
         else if (vertical == 0)
         {
             canMove = true;
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            switch (currentButton)
+            {
+                case 0: SceneManager.LoadScene("1Player");
+                    break;
+                case 1: SceneManager.LoadScene("2Player");
+                    break;
+                case 2: Application.Quit();
+                    break;
+            }
         }
     }
 }
